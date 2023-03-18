5e79fe1743d9d4c24bad36e854806eead64bb882
Mang <mangemmanuel07@gmail.com>
2023-03-18 15:01:25 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// Define a Function Using Function Declaration
+
+  function saturdayFun(someWhere="roller-skate") {
+    return `This Saturday, I want to ${someWhere}!`
+    }
+    saturdayFun();
+    
+//Define a Function Using a Function Expression
+    let mondayWork = function(willDo="go to the office"){
+        return `This Monday, I will ${willDo}.` 
+       }
+       mondayWork();
+
+       function wrapAdjective (style="*") {
+        return function(stringOne="special"){
+          return `You are ${style}${stringOne}${style}!`
+        }
+      }
+
+
+
